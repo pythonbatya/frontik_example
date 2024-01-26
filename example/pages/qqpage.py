cff6c94794c80b9a1014fea7d7602b30e8491be0
Leonid Vinogradov <l.vinogradov@hh.ru>
2024-01-26 15:45:33 +0300

    fastapi

--- example/pages/qqpage.py
+++ example/pages/qqpage.py
@@ -1,6 +1,45 @@
-import frontik.handler
+import asyncio
+
+from frontik.handler import PageHandler, self_getter
+from frontik.app import app_router, qq_path
+from fastapi import Depends, Request
 
 
-class Page(frontik.handler.PageHandler):
+class BasePage(PageHandler):
+    pass
+
+
+# ----------------------------------------------------------------------------------
+
+class Page(BasePage):
     async def get_page(self):
-        self.finish({123: 456})
+        result = await self.get_url('http://www.google.com', '/')
+
+        self.json.put({
+            'res1': result.status_code,
+        })
+
+# ----------------------------------------------------------------------------------
+
+
+# @app_router.get(qq_path())
+
+
+@app_router.get('/qqpage')
+async def get_version(self: Page = Depends(self_getter(Page))):
+    result = await self.get_url('http://www.google.com', '/')
+
+    self.json.put({
+        'res1': result.status_code,
+    })
+
+
+
+
+
+
+
+    # self.finish('qqqq\n')
+    # print(self)
+    # return {123: 456}
+
